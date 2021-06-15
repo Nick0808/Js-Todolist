38b7b6d6d0679fa1dfbdb31c78488611a59882aa
Nick0808 <chowj2154@gmail.com>
2021-06-15 22:59:24 +0900

    Read latitude and longitude

---
+++ weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "71b741a9374e65a1bc4cccb2acf38512";
+const COORDS = 'coords';
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+    console.log("can\'t access geo location");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCords = localStorage.getItem(COORDS);
+    if(loadedCords === null) {
+        askForCoords();
+    } else {
+
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
