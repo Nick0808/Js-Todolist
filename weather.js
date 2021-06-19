0d092485678c09ec936b8f641689fe17e1cc3a60
Nick0808 <chowj2154@gmail.com>
2021-06-19 22:36:28 +0900

    (Add Getting the weather API)

--- weather.js
+++ weather.js
@@ -1,5 +1,19 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY = "71b741a9374e65a1bc4cccb2acf38512";
-const COORDS = 'coords';
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+        ).then(function(response) { // 서버로 부터 데이터가 완전히 들어왔을 때 실행
+            return response.json();
+        })
+        .then(function(json) {
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature} | ${place}`;
+        });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +27,7 @@ function handleGeoSucces(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -23,17 +38,19 @@ function askForCoords() {
     navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
 }
 
-function loadCoords() {
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords === null) {
+function loadedCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
         askForCoords();
     } else {
-
+        const parsedCoords = JSON.parse(loadedCoords);
+        console.log(parsedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
 function init() {
-    loadCoords();
+    loadedCoords();
 }
 
 init();
